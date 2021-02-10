6a20938d638ba4e1ba73c56e5dea2724ebd04ea8
caitlin halphen <chalph3@lsu.edu>
2021-02-10 11:25:28 -0600

    Quiz

--- Quiz/end.js
+++ Quiz/end.js
@@ -53,5 +53,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/Quiz/highscores.html');
+    window.location.assign('/Portfolio/Quiz/highscores.html');
 };
