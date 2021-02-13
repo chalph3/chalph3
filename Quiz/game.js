1a22b9fb057e6d07ee6f52a95dab8fe52af4b48d
caitlin halphen <chalph3@lsu.edu>
2021-02-13 12:48:35 -0600

    quiz

--- Quiz/game.js
+++ Quiz/game.js
@@ -63,7 +63,7 @@ getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
-        return window.location.assign('/Portfolio/Quiz/end.html');
+        return window.location.assign('/Quiz/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
